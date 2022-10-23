739fa87fa3072763963f7e20ed73144dd2e37a61
Asus <panita23878@gmail.com>
2022-10-23 14:53:17 +0700

    update

--- detail.php
+++ detail.php
@@ -4,8 +4,8 @@
 <body>
 <?php
    $stmt = $pdo->prepare("SELECT * FROM member WHERE username = ?");
-   $stmt->bindParam(1, $_GET["username"]);
-   $stmt->execute();
+   $stmt->bindParam(1,$_GET["username"]);
+   $stmt->execute(); 
    $row = $stmt->fetch();
 ?>
 <div style="display:flex">
--- insert-member.php
+++ insert-member.php
@@ -1,17 +1,17 @@
 <?php include "connect.php" ?>
-<?php
-    $stmt = $pdo->prepare("INSERT INTO member VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bindParam(1, $_POST["username"]);
-    $stmt->bindParam(2, $_POST["password"]);
-    $stmt->bindParam(3, $_POST["name"]);
-    $stmt->bindParam(4, $_POST["address"]);
-    $stmt->bindParam(5, $_POST["mobile"]);
-    $stmt->bindParam(6, $_POST["email"]);
+<?php 
+    $stmt = $pdo->prepare("INSERT INTO member VALUES(?, ?, ?, ?, ?, ?)");
+    $stmt->bindParam(1,$_POST["username"]);
+    $stmt->bindParam(2,$_POST["password"]);
+    $stmt->bindParam(3,$_POST["name"]);
+    $stmt->bindParam(4,$_POST["address"]);
+    $stmt->bindParam(5,$_POST["mobile"]);
+    $stmt->bindParam(6,$_POST["email"]);
     $stmt->execute();
+
 ?>
 <html>
-<head><meta charset="UTF-8"></head>
+<head><meta charset="utf-8"></head>
 <body>
-    เพิ่มข้อมูลสมาชิกสำเร็จ
-</body>
+เพิ่มสมาชิกใหม่สำเร็จ</body>
 </html>
